4c08ccb7a858b6b48cfd9f8999456378e12ba262
Abby Pan <abbypan@gmail.com>
2014-01-07 11:34:44 +0800

    readme

---
+++ bin/convert_novel.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl 
+# require :  http://www.calibre-ebook.com/
+use strict;
+use warnings;
+
+my ($f, $type) = @ARGV;
+$type ||='mobi';
+
+my $dst_file = $f;
+$dst_file=~s/[a-z0-9]+$/$type/;
+
+my ($writer,$book) = $f=~/([^\\\/].*?)-(.+?)\.[^.]+$/;
+
+`ebook-convert "$f" "$dst_file" --authors "$writer" --title "$book"`;
