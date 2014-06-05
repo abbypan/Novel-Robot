9d0fd3f0af32eebb6a305d90d97c540d2d809378
Abby Pan <abbypan@gmail.com>
2014-06-05 13:20:45 +0800

    novel

--- bin/get_novel.pl
+++ bin/get_novel.pl
@@ -38,7 +38,7 @@ if($opt{f}){
 }
 
 if($items_ref){
-    my $select = $opt{m} ? $xs->select_item($info, $items_ref) : $items_ref;
+    my $select = $opt{E} ? $xs->select_item($info, $items_ref) : $items_ref;
     for my $r (@$select) {
         my $u = $r->{url};
         print "\rselect : $u\n";
@@ -52,44 +52,51 @@ sub read_option {
     $opt{s} = 'txt' if($opt{f});
 
     my %opt_out=(
+        board => $opt{B}, 
         book => $opt{u} ? decode(locale =>$opt{u}) :undef,
         category => $opt{c} ? [ split ',', decode( locale => $opt{c} ) ] : undef, 
+        chapter_ids => $opt{i} ? $xs->split_id_list($opt{i}) : undef, 
         chapter_regex => $opt{r} ? decode( locale => $opt{r} ) : undef, 
-        max_floor_num => $opt{F}, 
         max_process_num => $opt{p} // 3, 
-        min_word_num => $opt{N}, 
         only_poster => $opt{A}, 
         output => $opt{o}, 
+        packer_url => $opt{S}, 
+        passwd => $opt{P}, 
         query_keyword => $opt{k} ? decode( locale => $opt{k}) : undef, 
         query_type => $opt{q} ? decode( locale => $opt{q}) : undef, 
         remark => $opt{R} ? decode( locale => $opt{R} ) : undef, 
-        select_menu => $opt{m}, 
-        verbose => $opt{v} // 1, 
+        select_menu => $opt{E}, 
         site => $opt{s} || $opt{u} || $opt{b} ,
+        step_chapter_num => $opt{n}, 
         tag => $opt{T} ? [ split ',', decode( locale => $opt{T} ) ] : undef,
         type => $opt{t} || 'html', 
-        with_toc => $opt{C} // 1, 
-        writer => $opt{w} ? decode(locale => $opt{w}) : undef, 
-        packer_url => $opt{S}, 
         usr => $opt{U}, 
-        passwd => $opt{P}, 
-        chapter_ids => $opt{i} ? $xs->split_id_list($opt{i}) : undef, 
-        step_chapter_num => $opt{n}, 
-        board => $opt{B}, 
+        verbose  => $opt{v} // 1, 
+        with_toc => $opt{C} // 1, 
+        writer   => $opt{w} ? decode(locale => $opt{w}) : undef, 
     );
 
-    if($opt{i}){
-        @opt_out{qw/min_chapter_num max_chapter_num/} = split '-', $opt{i};
-    }
-
-    my ($class, $item) = $opt{b} ? qw/board item/ : qw/query item/ ;
+    # board -> 
+    my ($class, $item) = $opt{b} ? qw/board item/ : qw/query item/;
     if($opt{I}){
         ($opt_out{"min_${class}_page"}, $opt_out{"max_${class}_page"}) =
         split '-', $opt{I};
     }
+
     if($opt{M}){
         $opt_out{"max_${class}_${item}_num"} = $opt{M};
     }
+
+    # tiezi ->
+    if($opt{i}){
+        @opt_out{qw/min_tiezi_page max_tiezi_page/} = split '-', $opt{i};
+        @opt_out{qw/min_chapter_num max_chapter_num/} = split '-', $opt{i};
+    }
+
+    $opt_out{max_tiezi_floor_num} = $opt{m}; 
+
+    # floor ->
+    $opt_out{min_floor_word_num} = $opt{N}; 
 
     return %opt_out;
 }
@@ -106,7 +113,7 @@ sub get_novel_split {
         my $n = $m+$opt{step_chapter_num}-1;
         $n = $num if($n>$num);
 
-        my @info_list = ($info->{writer}, $info->{book}, $n)
+        my @info_list = ($info->{writer}, $info->{book}, $n);
         my $out = sprintf("%s-%s-%0${en}d.%s", 
             @info_list
             , $self->{packer}->suffix());
