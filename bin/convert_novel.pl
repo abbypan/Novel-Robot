06047b9f60706c9294fbfffd51cac8dc9a7f2246
abbypan <abbypan@gmail.com>
2014-06-08 13:39:40 +0800

    sendemail

--- bin/convert_novel.pl
+++ bin/convert_novel.pl
@@ -11,21 +11,40 @@ $|=1;
 #binmode(STDERR, ":encoding(console_out)");
 
 my %opt;
-getopt( 'ftwb', \%opt );
-$opt{t} ||= 'mobi';
+getopt( 'ftwbsdhup', \%opt );
 
-my $dst_file = $opt{f};
-$dst_file=~s/[a-z0-9]+$/$opt{t}/i;
+my $convert_file = convert_novel(%opt);
 
-my ($writer,$book) = $opt{f}=~/([^\\\/]+?)-(.+?)\.[^.]+$/;
-my %conv = (
-    'authors'=> $opt{w} || $writer , 
-    'title'=> $opt{b} || $book , 
-    'chapter-mark'=>"none" , 
-    'page-breaks-before'=>"/" , 
-    'max-toc-links'=>0, 
-);
+send_novel($convert_file, %opt) if($opt{d});
 
-my $conv_str = join(" ", map { qq[--$_ "$conv{$_}"] } keys(%conv) );
-my $cmd=qq[ebook-convert "$opt{f}" "$dst_file" $conv_str]; 
-system($cmd);
+sub convert_novel {
+    my ($f, %opt) = @_;
+    $opt{t} ||= 'mobi';
+
+    my $dst_file = $opt{f};
+    $dst_file=~s/[a-z0-9]+$/$opt{t}/i;
+
+    my ($writer,$book) = $opt{f}=~/([^\\\/]+?)-(.+?)\.[^.]+$/;
+    my %conv = (
+        'authors'=> $opt{w} || $writer , 
+        'title'=> $opt{b} || $book , 
+        'chapter-mark'=>"none" , 
+        'page-breaks-before'=>"/" , 
+        'max-toc-links'=>0, 
+    );
+
+    my $conv_str = join(" ", map { qq[--$_ "$conv{$_}"] } keys(%conv) );
+    my $cmd=qq[ebook-convert "$opt{f}" "$dst_file" $conv_str]; 
+    system($cmd);
+    return $dst_file;
+}
+
+sub send_novel {
+    my ($f, %opt) = @_;
+
+    my $cmd =qq[sendEmail -u '' -m 'novel' -f $opt{s} -t $opt{d} -a $f -vv];
+
+    $cmd.= qq[ -s $opt{h} -xu $opt{u} -xp $opt{p}] if($opt{h});
+
+    system($cmd);
+}
